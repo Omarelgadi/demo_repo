48b74677ed13289ed51232b9f592b26154b8c2ac
Omarelgadi <omarelgadi2004@gmail.com>
2025-04-28 18:59:47 +0200

    lectuer6

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home' , function(){
+// Route::get('/home' , function(){
    
-    return view('home');
-});
-Route::get('/loop',function(){
-    $cartypes = ['marcedes','BMW','Audi'];
-    return view('loop',compact('cartypes'));
+//     return view('home');
+// });
+// Route::get('/loop',function(){
+//     $cartypes = ['marcedes','BMW','Audi'];
+//     return view('loop',compact('cartypes'));
+// });
+Route::get('/category',function(){
+    $cat = Category::all();
+    return $cat;
 });
