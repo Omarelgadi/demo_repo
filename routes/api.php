4abe7ef1c7b8de783e218be05b62a1c950c1cf5d
Omarelgadi <omarelgadi2004@gmail.com>
2025-04-29 18:02:30 +0200

    adding models and apis

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,4 +12,24 @@ Route::get('/user', function (Request $request) {
 Route::get('/products',function(Request $request)
 {
     return Product::all();
+});
+
+
+Route::post('/product',function(Request $request){
+    $product = Product::create($request->all());
+    return $product;
+});
+
+Route::post('/category',function(Request $request){
+    $validator = Validator::make($request->all(),[
+        'name'=>'required|string|max:100',
+        'price'=>'required|numeric|min:0',
+    ]);
+
+
+    if($validator->fails()){
+        return response()->json(['error'=>$validator->errors()->first()],422);
+    }
+    $category=Category::create($request->all());
+    return $category;
 });
